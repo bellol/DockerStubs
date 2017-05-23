b4edf4b97912b6d65a91c10d1ec0d6bd897226d6
Wilhelm Eek <richard.eek@gmail.com>
2017-05-23 16:14:38 +1000

    Add Mockerfile implementation

--- server.js
+++ server.js
@@ -1,33 +1,14 @@
 'use strict';
-
 const express = require('express');
-
-// Constants
+const mocks = require('./Mockerfile');
+const app = express();
 const PORT = 8080;
 
-// App
-const app = express();
-
-const accounts = {
-    user: "Wilhelm",
-    accounts: [
-        {
-            type: "Smart Access",
-            balance: "-52135.87"
-        },
-        {
-            type: "Savings Maximiser",
-            balance: "0.42"
-        }
-    ]
+for (let mock of mocks.cases) {
+    app.get(mock.request.path, (req, res) => {
+        res.send(mock.response.body);
+    });
 }
-
-const users = {
-    users: ["Wilhelm", "Patrick", "Jim"]
-}
-
-app.get('/getAccounts', (req, res) => {res.send(accounts)});
-app.get('/getUsers', (req, res) => {res.send(users)});
 
 app.listen(PORT);
 console.log('Running on http://localhost:' + PORT);

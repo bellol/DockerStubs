8eb8267b1157b93c3a4e9d87a1d75e4f6f5f9b53
Wilhelm Eek <richard.eek@gmail.com>
2017-05-23 17:41:30 +1000

    Added POST support

--- server.js
+++ server.js
@@ -4,11 +4,32 @@ const mocks = require('./Mockerfile');
 const app = express();
 const PORT = 8080;
 
+// Helpers
+const quote = (text) => "\"" + text + "\"";
+
+// Register Calls
 for (let mock of mocks.cases) {
-    app.get(mock.request.path, (req, res) => {
-        res.send(mock.response.body);
-    });
+    switch (mock.request.type) {
+        case "GET":
+            console.log("SUCCESS:\tRegistered call of type " 
+                + quote(mock.request.type) + " and path " + quote(mock.request.path));
+            app.get(mock.request.path, (req, res) => res.send(mock.response.body));
+            break;
+
+        case "POST":
+            console.log("SUCCESS:\tRegistered call of type " 
+                + quote(mock.request.type) + " and path " + quote(mock.request.path));
+            app.post(mock.request.path, (req, res) => res.send(mock.response.body));
+            break;
+
+        default:
+            console.log("ERROR:\t\tCould not register call of type " 
+                + quote(mock.request.type) + " and path " + quote(mock.request.path));
+            break;
+    }
 }
 
+
+// Go
 app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
+console.log('DONE:\t\tListening on port ' + PORT);

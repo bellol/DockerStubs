08729a1e8833c60fb6c61a48d92b3b7197a0c2f5
Wilhelm Eek <richard.eek@gmail.com>
2017-06-03 20:33:54 +1000

    Added some more front-end code

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 export default class App extends React.Component {
-    render() {
-        return (
-            <div className="main-container">
-                <div className="mocker-logo">Mocker</div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="main-container">
+        <div className="new-stub-box"></div>
+        <div className="menu-box"></div>
+      </div>
+    );
+  }
 }
--- server.js
+++ server.js
@@ -10,38 +10,38 @@ const bodyParser = require('body-parser');
 const quote = (text) => "\"" + text + "\"";
 
 module.exports = {
-    app: function() {
-        const app = express();
-        
-        // Static Assets
-        const indexPath = path.join(__dirname, 'index.html');
-        const publicPath = express.static(path.join(__dirname, 'dist'));
-        app.use('/dist', publicPath);
+  app: function() {
+    const app = express();
+    
+    // Static Assets
+    const indexPath = path.join(__dirname, 'index.html');
+    const publicPath = express.static(path.join(__dirname, 'dist'));
+    app.use('/dist', publicPath);
 
-        app.get("/", (_, res) => {
-            res.sendFile(indexPath);
-        });
+    app.get("/", (_, res) => {
+      res.sendFile(indexPath);
+    });
 
-        app.post("/mockerAdd", (request, response) => {
-            switch(request.body.method) {
-                case "POST":
-                    app.post(request.body.endpoint, (req, res) => {
-                        if(_.isEqual(request.body.body, req.body)) res.send(request.body.response);
-                        else res.send("Bad Request");
-                    });
-                    break;
-                case "GET":
-                    app.get(request.body.endpoint, (req, res) => res.send(request.body.body));
-                    break;
-                default:
-                    response.send({
-                        
-                    });
-            }
-            response.send("Added " + request.body.method + " Endpoint");
-        });
-        return app;
-    }
+    app.post("/mockerAdd", (request, response) => {
+      switch(request.body.method) {
+        case "POST":
+          app.post(request.body.endpoint, (req, res) => {
+            if(_.isEqual(request.body.body, req.body)) res.send(request.body.response);
+            else res.send("Bad Request");
+          });
+          break;
+        case "GET":
+          app.get(request.body.endpoint, (req, res) => res.send(request.body.body));
+          break;
+        default:
+          response.send({
+            
+          });
+      }
+      response.send("Added " + request.body.method + " Endpoint");
+    });
+    return app;
+  }
 }
 
 
